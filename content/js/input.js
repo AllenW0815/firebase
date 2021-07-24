e48ff61c54686199d5d2b8fad279a5f74f94f19c
Allen <ro81516@gmail.com>
2021-07-25 01:01:59 +0800

    新增送出確認彈窗

--- content/js/input.js
+++ content/js/input.js
@@ -2,8 +2,34 @@
 const date = getElement('#date')
 const submitBtn = getElement('#submit')
 
+function makeSure() {
+    Swal.fire({
+        title: '確認內容無誤?',
+        text: "訂單源源不絕發大財",
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes'
+        }).then((result) => {
+        if (result.isConfirmed) {
+            writeOrder()
+            Swal.fire(
+            'success'
+            )
+        }
+    })
+}
+
+
+
+
 /* 日期自動生成 */
 date.value= moment().format('YYYY-MM-DD')
 
 /* 寫入 */
-submitBtn.addEventListener('click',writeOrder)
+submitBtn.addEventListener('click',makeSure)
+
+
+/* init */
+
