43d8a3558a4030f2fbbc88be3afdf42c01fdda76
Allen <ro81516@gmail.com>
2021-07-25 17:57:27 +0800

    新增初步的刪除功能

--- content/js/firebase_connect.js
+++ content/js/firebase_connect.js
@@ -68,6 +68,22 @@ const writeOrder = () => {
     });
 }
 
+// 刪除
+function deleteOrder(e) {;
+    e.stopPropagation();
+    const order = e.target.closest('tr')
+    const orderId = order.getAttribute('data-id')
+    let orderRef = db.collection("semicolon").doc("orders").collection(user).doc(orderId)
+
+    orderRef.delete()
+    .then(() => console.log("Document successfully deleted!"))
+    .then(() => getOrders())
+    .catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+}
+
+
 // 讀取顯示
 const getOrders = () => {
     // console.log(user);
@@ -75,7 +91,7 @@ const getOrders = () => {
 
     orderRef.get()
     .then((res) => {
-        let dataArray = res.docs.sort((a,b) => a.data().unix > b.data().unix ? 1 : -1)
+        let dataArray = res.docs.sort((a,b) => a.data().unix > b.data().unix ? -1 : 1)
         // console.log(dataArray[0].data());
         let profit =  0 
         const showOrders = getElement('#show_orders')
@@ -83,22 +99,32 @@ const getOrders = () => {
         let display = ''
         dataArray.forEach((doc) => {
             let item = doc.data()
-            display += `<tr>`
-            display += `<th>${item.date}</th>`
+            display += `<tr data-id="${doc.id}">`
+            display += `<th>
+            ${item.date}
+            <svg class="del_btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg>
+            </th>`
             display += `
             <td>${item.customer}</td>
             <td>${item.order_content}</td>
             <td>${item.price}</td>
             <td>${item.cost}</td>
             <td>${item.other_cost}</td>
-            <td>${item.cost-item.cost-item.other_cost}</td>`
+            <td>${item.price-item.cost-item.other_cost}</td>`
             display += `</tr>`
 
-            profit += item.cost-item.cost-item.other_cost
+            profit += item.price - item.cost - item.other_cost
         })
         showOrders.innerHTML = display
         showProfit.innerText =`$ ${profit}`
 
+    })
+    .then(() => {
+        const delBtn = getElements('.del_btn')
+        // console.log(delBtn);
+        delBtn.forEach((btn) => {
+            btn.addEventListener('click', deleteOrder)
+        })
     })
     .catch((error) => {
         console.log("Error getting document:", error);
